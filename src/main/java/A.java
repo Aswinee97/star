5667b04f813aba28cdbdc172c9c0c1db07af1404
Aswinee97 <imaswinee97@gmail.com>
2023-01-08 10:39:54 +0530

    aswinee

---
+++ src/main/java/A.java
@@ -0,0 +1,8 @@
+public class A{
+	public static void main(String []args) {
+		String str="Aswinee Kumar Behera";
+		for (int i = str.length()-1; i>=0 ; i--) {
+			System.out.println(str.charAt(i));
+		}
+	}
+}
